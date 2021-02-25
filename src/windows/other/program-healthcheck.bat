282ba4cd27174d0f246121653d71e1622be34564
Justintime50 <justinpaulhammond@gmail.com>
2021-02-25 09:39:04 -0700

    Update strings on program-healthcheck

--- src/windows/other/program-healthcheck.bat
+++ src/windows/other/program-healthcheck.bat
@@ -6,7 +6,7 @@
 EXIT /B 0
 
 :program_healthcheck
-    :: Checks if Valheim is running and restarts the service if not
+    :: Checks if a program is running and restarts it if not
     :: TODO: Don't print the output of finstr -> tasklist to console
     TASKLIST | FINDSTR "chrome" && echo Chrome's healthcheck passed || START "" "C:\Program Files\Google\Chrome\Application\chrome.exe" && echo Chrome's healthcheck failed, restarting...
     GOTO main
