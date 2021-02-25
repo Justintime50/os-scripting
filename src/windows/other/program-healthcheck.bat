67819e56e08fb8f7b1e907ea3256450e53e7296c
Justintime50 <justinpaulhammond@gmail.com>
2021-02-25 09:38:26 -0700

    Adds program healthcheck script to Windows

---
+++ src/windows/other/program-healthcheck.bat
@@ -0,0 +1,15 @@
+:main
+    :: Run healthcheck every 60 seconds
+    @echo off
+    timeout /t 60
+    GOTO program_healthcheck
+EXIT /B 0
+
+:program_healthcheck
+    :: Checks if Valheim is running and restarts the service if not
+    :: TODO: Don't print the output of finstr -> tasklist to console
+    TASKLIST | FINDSTR "chrome" && echo Chrome's healthcheck passed || START "" "C:\Program Files\Google\Chrome\Application\chrome.exe" && echo Chrome's healthcheck failed, restarting...
+    GOTO main
+EXIT /B 0
+
+call main
