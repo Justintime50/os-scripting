b07f20c034c606cdf36ab05e0a0e248f8e01872c
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-03-02 13:37:19 -0700

    Adding too many corpses script to macOS

---
+++ src/macos/maintenance/too-many-corpses.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Use this script if you encounter the "Opendirectoryd too many corpses being created" error on macOS
+# This script must be run from a recovery-mode terminal
+# Attribution: https://apple.stackexchange.com/questions/322509/opendirectoryd-too-many-corpses-being-created
+
+main() {
+    cd /Volumes/Macintosh\ HD/var/db/caches/opendirectory
+    mv ./mbr_cache ./mbr_cache-old
+    sudo shutdown -r
+}
+
+main
