8aebe77ad8eebb59a62652eb42829b76e0924fd5
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-03-02 13:43:01 -0700

    Fix shellcheck

--- src/macos/maintenance/too-many-corpses.sh
+++ src/macos/maintenance/too-many-corpses.sh
@@ -5,7 +5,7 @@
 # Attribution: https://apple.stackexchange.com/questions/322509/opendirectoryd-too-many-corpses-being-created
 
 main() {
-    cd /Volumes/Macintosh\ HD/var/db/caches/opendirectory
+    cd /Volumes/Macintosh\ HD/var/db/caches/opendirectory || exit 1
     mv ./mbr_cache ./mbr_cache-old
     shutdown -r now
 }
