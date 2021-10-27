9021f876f34a0f82fd00e7e95a04130461556c44
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-10-26 22:59:59 -0600

    fix: lint

--- src/macos/setup/setup-mailserver.sh
+++ src/macos/setup/setup-mailserver.sh
@@ -6,24 +6,24 @@
 main() {
     echo "Setting up mail server..."
     # Setup mail credentials
-    sudo bash -c "smtp.gmail.com:587 EMAIL:PASSWORD" > /etc/postfix/sasl_passwd
-    echo "Please fill in the email and password in /etc/postfix/sasl_passwd, press any button to continue"
+    echo "smtp.gmail.com:587 EMAIL:PASSWORD" | sudo tee /etc/postfix/sasl_passwd > /dev/null
+    echo "Please fill in the email and password in /etc/postfix/sasl_passwd, press any button to continue once this is done"
     read -rn 1
 
     # Make a backup of the config file
     sudo cp /etc/postfix/main.cf /etc/postfix/main.cf.bak
 
     # Write new config options to file
-    sudo bash -c {
-        relayhost '=' smtp.gmail.com:587
-        smtp_sasl_mechanism_filter '=' plain
-        smtp_sasl_auth_enable '=' yes
-        smtp_sasl_password_maps '=' hash:/etc/postfix/sasl_passwd
-        smtp_sasl_security_options '=' noanonymous
-        smtp_use_tls '=' yes
-        # smtp_tls_security_level '=' encrypt
-        # tls_random_source '=' dev:/dev/urandom
-    } > /etc/postfix/main.cf
+    sudo tee -a /etc/postfix/main.cf > /dev/null <<EOT
+relayhost = smtp.gmail.com:587
+smtp_sasl_mechanism_filter = plain
+smtp_sasl_auth_enable = yes
+smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
+smtp_sasl_security_options = noanonymous
+smtp_use_tls = yes
+# smtp_tls_security_level = encrypt
+# tls_random_source = dev:/dev/urandom
+EOT
 
     # Setup DB file and secure it (must be run only after the email/password have been set in `/etc/postfix/sasl_passwd`)
     sudo postmap /etc/postfix/sasl_passwd
