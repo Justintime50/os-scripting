6a93cbcb60897c7ef688c1f3aba8760ce22244cb
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-08-30 16:34:57 -0600

    feat: retry logic on uptimerobot healthcheck

--- src/macos/healthchecks/docker_uptimerobot_healthcheck.py
+++ src/macos/healthchecks/docker_uptimerobot_healthcheck.py
@@ -3,9 +3,10 @@ import subprocess
 import sys
 
 import requests
+from urllib3.util.retry import Retry
 
 # Check if your Docker instance is reachable by checking the overall uptime of your apps via UptimeRobot
-# Usage: UPTIME_ROBOT_API_KEY=123 venv/bin/python docker_uptimerobot_healthcheck.py
+# Usage: UPTIME_ROBOT_API_KEY=123 python3 docker_uptimerobot_healthcheck.py
 
 
 API_KEY = os.getenv('UPTIME_ROBOT_API_KEY')
@@ -14,24 +15,7 @@ REQUEST_TIMEOUT = 60
 
 
 def main():
-    url = "https://api.uptimerobot.com/v2/getMonitors"
-
-    payload = f"api_key={API_KEY}&format=json"
-    headers = {
-        "content-type": "application/x-www-form-urlencoded",
-        "cache-control": "no-cache",
-    }
-
-    try:
-        response = requests.request(
-            "POST",
-            url,
-            data=payload,
-            headers=headers,
-            timeout=REQUEST_TIMEOUT,
-        ).json()
-    except Exception as error:
-        sys.exit(error)
+    response = get_uptimerobot_monitors()
 
     monitors = response["monitors"]
     num_of_monitors = len(monitors)
@@ -45,11 +29,53 @@ def main():
         print('UptimeRobot healthcheck passed.')
 
 
+def get_uptimerobot_monitors():
+    """Get the monitors from UptimeRobot.
+
+    UptimeRobot is notoriously flakey (timing out or throwing 5xx errors), retry on failure before giving up.
+    """
+    retry_strategy = Retry(
+        total=3,
+        backoff_factor=1,
+        status_forcelist=[
+            429,
+            500,
+            502,
+            503,
+            504,
+        ],
+        allowed_methods=["POST"],
+    )
+    requests_http_adapter = requests.adapters.HTTPAdapter(max_retries=retry_strategy)
+    requests_session = requests.Session()
+    requests_http_adapter = requests.adapters.HTTPAdapter(max_retries=retry_strategy)
+    requests_session.mount(prefix="https://", adapter=requests_http_adapter)
+
+    url = "https://api.uptimerobot.com/v2/getMonitors"
+    payload = f"api_key={API_KEY}&format=json"
+    headers = {
+        "content-type": "application/x-www-form-urlencoded",
+        "cache-control": "no-cache",
+    }
+
+    try:
+        response = requests.request(
+            "POST",
+            url,
+            data=payload,
+            headers=headers,
+            timeout=REQUEST_TIMEOUT,
+        )
+    except Exception as error:
+        sys.exit(error)
+
+    return response.json()
+
+
 def restart_docker():
     try:
         subprocess.run(
-            'killall Docker && killall "Docker Desktop" && killall com.docker.backend && sleep 10 && open'
-            ' /Applications/Docker.app',  # noqa
+            'killall Docker && killall "Docker Desktop" && killall com.docker.backend && sleep 10 && open /Applications/Docker.app',  # noqa
             stderr=subprocess.STDOUT,
             check=True,
             shell=True,
