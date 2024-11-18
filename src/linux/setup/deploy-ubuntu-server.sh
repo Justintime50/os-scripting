152f5b856412d9e3c2b5175fbe808ed4ad4b8842
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2024-11-18 16:51:47 -0700

    fix: ubuntu setup scripts

--- src/linux/setup/deploy-ubuntu-server.sh
+++ src/linux/setup/deploy-ubuntu-server.sh
@@ -8,41 +8,41 @@ read -rs PASSWORD
 
 # Update packages
 echo "Updating packages..."
-echo "$PASSWORD" | sudo apt update && sudo apt upgrade -y
+echo "$PASSWORD" | sudo -S apt update && sudo -S apt upgrade -y
 
 # Install Docker
 echo "Installing Docker..."
-echo "$PASSWORD" | sudo snap remove docker
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do echo "$PASSWORD" | sudo apt-get remove $pkg; done
-echo "$PASSWORD" | sudo apt-get install ca-certificates curl
-echo "$PASSWORD" | sudo install -m 0755 -d /etc/apt/keyrings
-echo "$PASSWORD" | sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-echo "$PASSWORD" | sudo chmod a+r /etc/apt/keyrings/docker.asc
-# TODO: sudo doesn't get the password piped in here, is that necessary?
+echo "$PASSWORD" | sudo -S snap remove docker
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do echo "$PASSWORD" | sudo -S apt-get remove $pkg; done
+echo "$PASSWORD" | sudo -S apt-get install ca-certificates curl
+echo "$PASSWORD" | sudo -S install -m 0755 -d /etc/apt/keyrings
+echo "$PASSWORD" | sudo -S curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+echo "$PASSWORD" | sudo -S chmod a+r /etc/apt/keyrings/docker.asc
+# TODO: sudo -S doesn't get the password piped in here, is that necessary?
 echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-echo "$PASSWORD" | sudo apt-get update
-echo "$PASSWORD" | sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo -S tee /etc/apt/sources.list.d/docker.list >/dev/null
+echo "$PASSWORD" | sudo -S apt-get update
+echo "$PASSWORD" | sudo -S apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Fix Docker permissions
-echo "$PASSWORD" | sudo usermod -aG docker "$USER"
+echo "$PASSWORD" | sudo -S usermod -aG docker "$USER"
 
 # Start Docker on boot
-echo "$PASSWORD" | sudo systemctl enable docker
-echo "$PASSWORD" | sudo systemctl start docker
+echo "$PASSWORD" | sudo -S systemctl enable docker
+echo "$PASSWORD" | sudo -S systemctl start docker
 
 # Setup firewall
 echo "Setting up firewall..."
-echo "$PASSWORD" | sudo ufw allow OpenSSH
-echo "$PASSWORD" | sudo ufw allow 80
-echo "$PASSWORD" | sudo ufw allow 443
-echo "$PASSWORD" | sudo ufw enable
+echo "$PASSWORD" | sudo -S ufw allow OpenSSH
+echo "$PASSWORD" | sudo -S ufw allow 80
+echo "$PASSWORD" | sudo -S ufw allow 443
+echo "$PASSWORD" | sudo -S ufw enable
 
 # Install other packages
 echo "Installing other packages..."
-echo "$PASSWORD" | sudo apt install -y \
+echo "$PASSWORD" | sudo -S apt install -y \
     composer \
     fail2ban \
     just \
@@ -57,11 +57,16 @@ echo "Installing Homebrew..."
 
 # Disable IOMMU if running on a Mac
 # https://www.reddit.com/r/linux_on_mac/comments/w3hisc/network_dropout_fix_for_linux_on_mac_with_kernel/
-if echo "$PASSWORD" | sudo dmidecode -s system-manufacturer | grep -qi "apple"; then
+if echo "$PASSWORD" | sudo -S dmidecode -s system-manufacturer | grep -qi "apple"; then
     echo "Disabling IOMMU..."
-    echo "$PASSWORD" | sudo sed -i 's/^GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="iommu.passthrough=1/' /etc/default/grub
-    echo "$PASSWORD" | sudo update-grub2
+    echo "$PASSWORD" | sudo -S sed -i 's/^GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="iommu.passthrough=1/' /etc/default/grub
+    echo "$PASSWORD" | sudo -S update-grub2
 fi
+
+# Setup directories
+echo "Setting up directories..."
+mkdir -p git
+mkdir -p /mnt/usb
 
 # Change shell
 echo "Changing shell..."
@@ -77,4 +82,4 @@ read -rn 1
 echo "Rebooting..."
 sleep 5
 history -c
-echo "$PASSWORD" | sudo reboot
+echo "$PASSWORD" | sudo -S reboot
---
+++ src/linux/setup/setup-ubuntu-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+brew tap justintime50/formulas
+brew install cronic
+brew install srvinfra
+brew install keychain
+brew install alchemist
